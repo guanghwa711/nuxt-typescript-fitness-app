27001e0de50c83c51847d4bed9d9e9778304dde4
guanghwa711 <guanghwa711@gmail.com>
2022-05-24 08:28:03 +0300

    create admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="admin-layout">
+    <header class="header">
+      <div class="container">
+        <nuxt-link class="logo" to="/">
+          <img src="~/assets/images/logo.svg" alt="" />
+        </nuxt-link>
+      </div>
+    </header>
+    <aside class="aside">
+      <Sidebar class="sidebar" :visible="true" :modal="false" :showCloseIcon="false" :autoZIndex="false">
+        <div class="sidebar__inner">
+          <Menu class="sidebar__menu" ref="menu" :model="items" />
+        </div>
+      </Sidebar>
+    </aside>
+    <div class="admin-page container">
+      <NuxtChild />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from 'primevue/sidebar'
+import Menu from 'primevue/menu'
+
+const items = [
+  {
+    label: 'Статистика',
+    icon: 'pi pi-chart-line',
+    to: '/admin'
+  },
+  {
+    label: 'Заказы',
+    icon: 'pi pi-money-bill',
+  },
+  {
+    label: 'Бренды',
+    icon: 'pi pi-briefcase',
+  },
+  {
+    label: 'Категории',
+    icon: 'pi pi-tags',
+    url: "https://vuejs.org/",
+  },
+  {
+    label: 'Товары',
+    icon: 'pi pi-shopping-cart',
+    to: "/fileupload",
+  },
+];
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/variables";
+
+.header {
+  background-color: $dark-gray;
+  width: 100%;
+  display: inline-block;
+  padding: 15px 0;
+  position: fixed;
+  z-index: 1000;
+}
+.sidebar {
+  &__inner {
+    padding-top: 50px;
+  }
+}
+.admin-page {
+  padding-top: 80px;
+  padding-left: 300px;
+}
+</style>
---
+++ pages/admin.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<nuxt-child />
+	</div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'admin',
+})
+</script>

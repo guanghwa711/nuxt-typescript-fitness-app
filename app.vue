a4ce58618c1c9606fc86494c54b96439aa16d9c7
guanghwa711 <guanghwa711@gmail.com>
2022-05-24 09:42:38 +0300

    add ScrollTop component

--- app.vue
+++ app.vue
@@ -1,9 +1,20 @@
 <template>
 	<div id="app">
     <nuxt-layout />
+    <ScrollTop class="scroll-top"/>
   </div>
 </template>
 
 <script setup>
-
+import ScrollTop from 'primevue/scrolltop';
 </script>
+
+<style lang="scss" scoped>
+@import '/assets/styles/_variables';
+.scroll-top {
+  background: rgba($red, .7);
+  &:hover {
+    background: rgba($red, .8);
+  }
+}
+</style>

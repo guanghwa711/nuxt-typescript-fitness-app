8e376368b33a2b0096573b869c1afacb4ced67ac
guanghwa711 <guanghwa711@gmail.com>
2022-05-26 15:46:57 +0300

    delete primevue

--- app.vue
+++ app.vue
@@ -1,20 +1,5 @@
 <template>
-	<div id="app">
+  <div id="app">
     <nuxt-layout />
-    <ScrollTop class="scroll-top"/>
   </div>
 </template>
-
-<script setup>
-import ScrollTop from 'primevue/scrolltop';
-</script>
-
-<style lang="scss" scoped>
-@import '/assets/styles/_variables';
-.scroll-top {
-  background: rgba($red, .7);
-  &:hover {
-    background: rgba($red, .8);
-  }
-}
-</style>

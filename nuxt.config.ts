718f540c7a151d9b4c1da7cc6a60ea39888562b2
guanghwa711 <guanghwa711@gmail.com>
2022-05-26 15:47:10 +0300

    add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,9 +18,17 @@ export default defineNuxtConfig({
 	},
 
 	css: [
-		'primevue/resources/themes/saga-blue/theme.css',
-    'primevue/resources/primevue.css',
-    'primeicons/primeicons.css',
+		'mdi/css/materialdesignicons.min.css',
+		'vuetify/lib/styles/main.sass',
 		'/assets/styles/index.scss'
 	],
+
+	build: {
+		transpile: ['vuetify'],
+	},
+	vite: {
+		define: {
+			'process.env.DEBUG': false,
+		},
+	},
 })

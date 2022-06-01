9e994334a55077aa9d69b69624f4f7097ed66411
guanghwa711 <guanghwa711@gmail.com>
2022-06-01 19:49:02 +0300

    add button component

---
+++ components/My/Button.vue
@@ -0,0 +1,29 @@
+<template>
+	<button class="btn" :class="{ disabled: disabled }" :type="type" :disabled="disabled">
+		<slot />
+	</button>
+</template>
+
+<script setup>
+const props = defineProps({
+	type: {
+		type: String,
+		default: () => 'button'
+	},
+	disabled: {
+		type: Boolean,
+		default: () => false
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/styles/variables';
+.btn {
+	padding: 12px 70px;
+	&.disabled {
+		cursor: default;
+		background-color: $gray-primary;
+	}
+}
+</style>

9e3cd3635d874dd125d01fc62e7696bc0d088852
guanghwa711 <guanghwa711@gmail.com>
2022-06-03 23:23:59 +0300

    add toolbar component

---
+++ components/Admin/Toolbar.vue
@@ -0,0 +1,38 @@
+<template>
+			<div class="toolbar">
+			<v-text-field class="toolbar__field" v-model="field" label="Поиск"></v-text-field>
+			<v-btn class="toolbar__add" color="success" @click="add">добавить
+				<v-icon>mdi-plus</v-icon>
+			</v-btn>
+		</div>
+</template>
+
+<script setup>
+const emit = defineEmits(['add', 'update:searchField'])
+
+const field = ref('')
+const add = () => {
+	emit('add')
+}
+watch(field, (value) => {
+	emit('update:searchField', value)
+})
+</script>
+
+<style lang="scss" scoped>
+.toolbar {
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 20px;
+
+	&__field {
+		max-width: 200px;
+	}
+
+	&__add {
+		display: flex;
+		align-items: center;
+		gap: 10px;
+	}
+}
+</style>
